92378ae7b164ca82f42a1c5f1e5b8e6d8e2c2129
Jessy Castillo <jessycas.g16@gmail.com>
2022-03-20 23:45:47 -0400

    usar puerto

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || '3000'
 
-app.listen(3000, () => {
-console.log("Escuchando el puerto 3000")
+app.listen(port, () => {
+console.log("Escuchando el puerto "+port)
 })
 
 app.use(express.static("public"))
