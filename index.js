2c7bd867e65448b9f05adee44baf7a64a763c960
Jessy Castillo <jessycas.g16@gmail.com>
2022-04-09 23:08:15 -0400

    Agregar sistema de plantillas y curriculum

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require("express")
 
 const app = express()
 const port = process.env.PORT || '3000'
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res) {
+    res.render("curriculum")
+})
 
 app.listen(port, () => {
 console.log("Escuchando el puerto "+port)
 })
 
-app.use(express.static("public"))
+
