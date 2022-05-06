2f8c655415d31f431f904303de4029fe3317551b
Jessy Castillo <jessycas.g16@gmail.com>
2022-05-05 22:37:24 -0400

    Guardar contacto

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require("express")
-
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || '3000'
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+const sqlite3 = require('sqlite3')
+
+const db = new sqlite3.Database('database.sqlite')
+db.run("CREATE TABLE IF NOT EXISTS contactos (nombre VARCHAR, email VARCHAR, comentario TEXT)")
+
+
+
+
 
 app.get('/', function(req,res) {
     res.render("curriculum")
 })
+
+app.get('/contacto', function(req,res) {
+    res.render("contacto")
+})
+
+app.post('/guardar', function(req,res) {
+    
+    const nombre = req.body.name
+    const email = req.body.email
+    const comentario = req.body.comentario
+    console.log(req.body.name)
+    res.redirect('/contacto')
+
+})
+
 
 app.listen(port, () => {
 console.log("Escuchando el puerto "+port)
